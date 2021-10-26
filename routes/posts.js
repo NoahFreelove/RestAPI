c0131dff7cf4a246c80a6582fe582f6665c7f538
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2021-10-26 10:08:39 -0400

    Post page displays all posts in json format

--- routes/posts.js
+++ routes/posts.js
@@ -2,23 +2,33 @@ const express = require('express');
 const Post = require('../models/Post')
 const router = express.Router();
 
-router.get('/',(req, res) => {
-        res.send("Post Home Pages")
+router.get('/', async(req, res) => {
+        try {
+            const posts = await Post.find();
+            res.json(posts);
+        }catch (e)
+        {
+            res.json({message: e});
+        }
     });
 
-router.post("/", (req, res) =>{
+router.post("/", async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description,
         userId: req.body.userId
     });
 
-    post.save().then(data=>{
-        res.json(data + "saved")
-    })
-        .catch(err=>{
-            res.json({message: err})
-        });
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    }catch (e)
+    {
+        res.json({message: e});
+    }
+
+
+
 });
 
 module.exports = router;
