f24dd1ad22da6fe50947aeae8ca7871811246947
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2021-11-08 14:56:20 -0500

    Added Access Control Allow Origin

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const {mongo} = require("mongoose");
 const bodyParser = require("body-parser")
+const cors = require('cors')
 
 require('dotenv/config')
 
 const app = express();
 const port = process.env.PORT || 3000;
 const postsRoute = require('./routes/posts');
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/posts', postsRoute);
+
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('To view a post go to /posts/');
